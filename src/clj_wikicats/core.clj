423a41509703172b73aadec6c9fbd46fe22c34fe
Hagen Tönnies <hagen.toennies@geektanklabs.de>
2014-11-19 20:33:52 +0100

    Algo experiments

--- src/clj_wikicats/core.clj
+++ src/clj_wikicats/core.clj
@@ -1,13 +1,14 @@
 (ns clj-wikicats.core
   (:import (java.io FileReader))
   (require [clojure.data.json :as json]
-            [loom.graph :refer :all]))
+            [loom.graph :refer :all]
+            [loom.alg :refer :all]))
 
 (def category-graph (atom (digraph)))
 
 (defn build-german-cat-graph [path-to-json]
   (let [raw-json (slurp (FileReader. path-to-json))
-        k1upper-cats (json/read-str raw-json :key-fn keyword)]
+        k1upper-cats (json/read-str raw-json)]
     (doseq [[from k1-uppers] k1upper-cats]
       (loop [upper-cat k1-uppers
              graph @category-graph]
@@ -19,13 +20,12 @@
 
 (defn -main [& args]
   (println "Start")
-  (build-german-cat-graph "/Users/hagentonnies/workspace/clj-wikicats/resources/de_cat_graph.json")
+  (build-german-cat-graph "/Users/hagen/workspace/clj-wikicats/resources/de_cat_graph.json")
 
   (println "Node Count" (count (nodes @category-graph)))
-  (print "Nodes")
-  (doseq [n (nodes @category-graph)]
-    (println n))
-  (print "Edges")
-  (doseq [n (edges @category-graph)]
-    (println n))
+  (println "Elefant_in_der_Kunst -> !Hauptkategorie" (time (astar-path @category-graph "Elefant_in_der_Kunst"  "!Hauptkategorie" (fn [x y] 0))) )
+  (println "Elefant_in_der_Kunst -> !Hauptkategorie" (time (shortest-path @category-graph "Elefant_in_der_Kunst"  "!Hauptkategorie") ) )
+  (println "Angela_Merkel ->  Sachsystematik" (time (astar-path @category-graph "Angela_Merkel"  "Sachsystematik" (fn [x y] 0))))
+  (println "Polymerbildende_Reaktion -> Sachsystematik" (time (shortest-path @category-graph "Polymerbildende_Reaktion"  "Sachsystematik") ) )
+  (println "Data-Mining -> !Hauptkategorie"(time (shortest-path @category-graph "Data-Mining"  "!Hauptkategorie")) )
   (println "end!"))
